b2adbdb95fc8da67f9b186b0c220f2203bad295d
Sumit kumar <manojkumar90123281@gmail.com>
2026-05-28 22:44:37 +0530

    Create multiplication_table.c

---
+++ Day_1/multiplication_table.c
@@ -0,0 +1,11 @@
+// WAP to print multiplication table of a given number
+#include <stdio.h>
+int main(){
+    int i,N;
+    printf("Enter the value of N:");
+    scanf("%d",&N);
+    for(i=1;i<=10;i++){
+        printf("%d×%d=%d \n",N,i,N*i);
+    }
+    return 0;
+}
