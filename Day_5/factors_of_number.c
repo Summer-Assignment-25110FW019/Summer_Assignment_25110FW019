e11f0c701d650d674b33a693cf43730565f4f7d2
Sumit kumar <manojkumar90123281@gmail.com>
2026-06-04 18:07:23 +0530

    Create factors_of_number.c

---
+++ Day_5/factors_of_number.c
@@ -0,0 +1,15 @@
+// WAP to print facors of a number
+#include <stdio.h>
+int main() {
+    int i,n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    printf("The factors of %d are:",n);
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            printf(" %d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
