ce8a09b5b93a1eabd6b63109b46382abf1b1ba63
Sumit kumar <manojkumar90123281@gmail.com>
2026-06-06 19:15:17 +0530

    Create recursive_reverse_number.c

---
+++ Day_7/recursive_reverse_number.c
@@ -0,0 +1,17 @@
+// WAP to recursive reverse number
+#include <stdio.h>
+int rev=0;
+int reverse(int n){
+    if(n==0){
+        return rev;
+    }
+    rev = rev*10+n%10;
+    reverse(n/10);
+}
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Reverse of a number is: %d ", reverse(n));
+    return 0;
+}
