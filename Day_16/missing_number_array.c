1443aabf1ecc1c28dd1ae0b2d2df425b0271962f
Sumit kumar <manojkumar90123281@gmail.com>
2026-06-15 21:32:43 +0530

    Create missing_number_array.c

---
+++ Day_16/missing_number_array.c
@@ -0,0 +1,17 @@
+// WAP to find missing number in array
+#include <stdio.h>
+int main(){
+    int arr[100],n,i;
+    int sum=0,total=0,missing;
+    printf("Enter value of n: ");
+    scanf("%d",&n);
+    printf("Enter %d elements:\n", n - 1);
+    for(i=0;i<n-1;i++){
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    total=n*(n+1)/2;
+    missing=total-sum;
+    printf("Missing number is %d", missing);
+    return 0;
+}
