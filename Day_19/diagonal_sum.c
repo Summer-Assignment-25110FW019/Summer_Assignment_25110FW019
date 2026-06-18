81661fddeaeb085e404b43207be17249a7f236ec
Sumit kumar <manojkumar90123281@gmail.com>
2026-06-18 21:01:25 +0530

    Add diagonal sum calculation for a matrix
    Implement a program to calculate the diagonal sum of a square matrix.

---
+++ Day_19/diagonal_sum.c
@@ -0,0 +1,18 @@
+// WAP to find diagonal sum of a matrix
+#include <stdio.h>
+int main(){
+    int a[10][10],n,i,j,sum=0;
+    printf("Enter order of square matrix: ");
+    scanf("%d",&n);
+    printf("Enter matrix elements:\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        sum=sum+a[i][i];
+    }
+    printf("Diagonal Sum is: %d",sum);
+    return 0;
+}
