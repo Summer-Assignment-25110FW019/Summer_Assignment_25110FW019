e5ff9d5f95030f3710ea0f2d6c93b94e17fd92b7
Sumit kumar <manojkumar90123281@gmail.com>
2026-06-25 20:36:18 +0530

    Add voter eligibility program in C

---
+++ Day_26/voter_eligibility.c
@@ -0,0 +1,14 @@
+//WAP to create voter eligibility
+#include <stdio.h>
+int main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+    if(age>=18){
+        printf("Eligible for Voting");
+    }
+    else{
+        printf("Not Eligible for Voting");
+    }
+    return 0;
+}
